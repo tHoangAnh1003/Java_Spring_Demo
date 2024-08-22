4752ba35934913aec5b692ccf6ff055f670ba5ff
tHoangAnh1003 <t.hoang.1003.anh@gmail.com>
2024-08-22 15:08:38 +0700

    clean code

--- spring-boot/src/main/java/com/javaweb/service/BuildingService.java
+++ spring-boot/src/main/java/com/javaweb/service/BuildingService.java
@@ -8,5 +8,5 @@ import com.javaweb.builder.BuildingSearchBuilder;
 
 public interface BuildingService {
 	List<BuildingDTO> findAll(Map<String, Object> ob, List<String> typeCode);
-	public boolean existsById(Long id);
+	boolean existsById(Long id);
 }
